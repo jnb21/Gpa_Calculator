39301370fdff160b68c3ef05199be85ac65ac415
Jordan <bikong@mac.home>
2026-03-28 10:46:01 -0400

    Finished version2. Just need to test and merge.

---
+++ src/pack/CourseNotFoundException.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class CourseNotFoundException extends Exception{
+
+    public CourseNotFoundException (String message){
+        super(message);
+    }
+
+    public CourseNotFoundException (){
+        super("This course does not exist");
+    }
+}
